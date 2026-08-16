5507ecf1e8a83269f82ba6c783cd86270891d7b9
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-08-17 00:33:15 +0500

    Compilation fixes under GS.

--- CoreData/NSManagedObjectModel.m
+++ CoreData/NSManagedObjectModel.m
@@ -9,7 +9,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #import <CoreData/NSManagedObjectModel.h>
 #import <CoreData/NSEntityDescription.h>
 
-#import <Foundation/NSKeyedUnarchiver.h>
+#import <Foundation/Foundation.h>
 #import "CoreDataUtilities.h"
 
 @implementation NSManagedObjectModel
